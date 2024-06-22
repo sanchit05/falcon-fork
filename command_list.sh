6817193996b1c8ecd112a3b4f4a7c98fd45f0ef1
Arpan Adlakhiya <arpan.adlakhiya@npci.org.in>
2024-06-22 11:19:44 +0530

    added sections

--- command_list.sh
+++ command_list.sh
@@ -3,6 +3,11 @@
 # install filestore.
 helm upgrade --install --atomic --timeout 300s --wait-for-jobs filestore -n filestore helm-charts/filestore/ -f examples/filestore/values.yaml --create-namespace
 
+#verify
+curl http://filestore.my-hlf-domain.com:31862
+
+#================================================================
+
 # create orderer NS
 kubectl create ns orderer
 
@@ -11,6 +16,11 @@ kubectl -n orderer create secret generic rca-secret --from-literal=user=rca-admi
 
 # install root ca
 helm upgrade --install --atomic --timeout 300s --wait-for-jobs root-ca -n orderer helm-charts/fabric-ca -f examples/fabric-ca/root-ca.yaml
+
+# verify
+curl https://root-ca.my-hlf-domain.com:30448/cainfo --insecure
+
+#================================================================
 
 # create secret for TLS ca
 kubectl -n orderer create secret generic tlsca-secret --from-literal=user=tls-admin --from-literal=password=TlsComplexPassword
@@ -24,6 +34,7 @@ helm upgrade --install --atomic --timeout 300s --wait-for-jobs rootca-ops -n ord
 # tls ca identities
 helm upgrade --install --atomic --timeout 300s --wait-for-jobs tlsca-ops -n orderer helm-charts/fabric-ops/ -f examples/fabric-ops/tlsca/tlsca-identities.yaml
 
+#================================================================================================
 # create secret for ica-orderer
 kubectl -n orderer create secret generic orderer-secret --from-literal=user=ica-orderer --from-literal=password=icaordererSamplePassword
 
@@ -45,20 +56,28 @@ helm upgrade --install --atomic --timeout 300s --wait-for-jobs orderer-ops -n or
 # register org1 identities
 helm upgrade --install --atomic --timeout 300s --wait-for-jobs org1-ops -n org1 helm-charts/fabric-ops/ -f examples/fabric-ops/org1/identities.yaml
 
+#================================================================
+
 # Generate Genesisblock & Channel transaction file
 helm upgrade --install --atomic --timeout 300s --wait-for-jobs cryptogen -n orderer helm-charts/fabric-ops/ -f examples/fabric-ops/orderer/orderer-cryptogen.yaml
 
 # deploy orderers
 helm upgrade --install --atomic --timeout 300s --wait-for-jobs orderer -n orderer helm-charts/fabric-orderer/ -f examples/fabric-orderer/orderer.yaml
 
+#================================================
+
 # deploy org1 peers
 helm upgrade --install --atomic --timeout 300s --wait-for-jobs peer -n org1 helm-charts/fabric-peer/ -f examples/fabric-peer/org1/values.yaml
+
+#================================================
 
 # create channel
 helm upgrade --install --atomic --timeout 300s --wait-for-jobs channelcreate -n org1 helm-charts/fabric-ops/ -f examples/fabric-ops/org1/channel-create.yaml
 
 # update anchor peer
 helm upgrade --install --atomic --timeout 300s --wait-for-jobs updateanchorpeer -n org1 helm-charts/fabric-ops/ -f examples/fabric-ops/org1/update-anchor-peer.yaml
+
+#================================================================
 
 # copy chaincode
 filestore_pod=$(kubectl get pods -n filestore -l app.kubernetes.io/name=filestore -o jsonpath="{.items[*].metadata.name}")
